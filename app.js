2e3cc35c911298f6f235d7657ca23ede83a95b05
Ashu <ashutoshm90@gmail.com>
2016-02-18 02:37:02 +0530

    db

--- app.js
+++ app.js
@@ -25,39 +25,69 @@ app.get('/about', function(req,res){
 });
 
 app.get('/todos', function(req,res){
-    var queryParams = req.query;
-    var filteredTodos = todos;
-    if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
-        filteredTodos = _.where(filteredTodos, {completed: true});
-    }else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
-        filteredTodos = _.where(filteredTodos, {completed: false});
+    var query = req.query;
+
+    var where = {};
+    if(query.hasOwnProperty('completed') && query.completed === 'true'){
+        where.completed = true;
+    }else if(query.hasOwnProperty('completed') && query.completed === 'false'){
+        where.completed = false;
     }
 
-    if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0){
-        filteredTodos = _.filter(filteredTodos, function(todo){
-            return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-        });
+    if(query.hasOwnProperty('q') && query.q.length > 0){
+        where.description = {
+            $like: '%' + query.q + '%'
+        };
     }
-   res.json(filteredTodos);
+
+    db.todo.findAll({where:where}).then(function(todos){
+        res.json(todos);
+    },function(e){
+        res.status(500).send();
+    });
+   // var filteredTodos = todos;
+   // if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
+   //     filteredTodos = _.where(filteredTodos, {completed: true});
+   // }else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
+   //     filteredTodos = _.where(filteredTodos, {completed: false});
+   // }
+   //
+   // if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0){
+   //     filteredTodos = _.filter(filteredTodos, function(todo){
+   //         return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+   //     });
+   // }
+   //res.json(filteredTodos);
 });
 
 app.get('/todos/:id', function(req,res){
-    var todoId = req.params.id;
-    var mtodo;
+    var todoId = parseInt(req.params.id, 10);
+    db.todo.findById(todoId).then(function(todo){
+        if(!!todo){
+            res.json(todo.toJSON());
+        }else{
+            res.status(404).send();
+        }
+    }, function(e){
+       res.status(500).json(e);
+    });
 
-    mtodo = _.findWhere(todos, {id:todoId});
 
-    //todos.forEach(function(todo){
-    //   if(parseInt(todoId, 10) === todo.id){
-    //       mtodo = todo;
-    //   }
-    //});
-
-    if(mtodo){
-        res.json(mtodo);
-    }else{
-        res.status(404).send();
-    }
+    //var mtodo;
+    //
+    //mtodo = _.findWhere(todos, {id:todoId});
+    //
+    ////todos.forEach(function(todo){
+    ////   if(parseInt(todoId, 10) === todo.id){
+    ////       mtodo = todo;
+    ////   }
+    ////});
+    //
+    //if(mtodo){
+    //    res.json(mtodo);
+    //}else{
+    //    res.status(404).send();
+    //}
 
 
 });
@@ -87,14 +117,30 @@ app.post('/todos', function(req,res){
 
 app.delete('/todos/:id', function(req,res){
    var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoId});
 
-    if(!matchedTodo){
-        res.status(404).json({"error": "no json found with that id"});
-    }else{
-        todos = _.without(todos,matchedTodo);
-        res.json(matchedTodo);
-    }
+    db.todo.destroy({
+        where: {
+            id:todoId
+        }
+    }).then(function(rowsDeleted){
+        if(rowsDeleted === 0){
+            res.status(404).json({
+                "error": "No todo With That id"
+            });
+        }else{
+            res.status(204).send();
+        }
+    },function(e){
+        res.status(500).send();
+    })
+    //var matchedTodo = _.findWhere(todos, {id: todoId});
+    //
+    //if(!matchedTodo){
+    //    res.status(404).json({"error": "no json found with that id"});
+    //}else{
+    //    todos = _.without(todos,matchedTodo);
+    //    res.json(matchedTodo);
+    //}
 });
 
 app.put('/todos/:id', function(req,res){
